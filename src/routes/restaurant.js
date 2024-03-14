f67b14d4a42c428a265c45f43b3724ce05630063
BrandonJung <49972155+BrandonJung@users.noreply.github.com>
2024-03-13 19:01:17 -0700

    Retrieve restaurant by id, update restaurant field

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   addRestaurant,
   deleteAllRestaurants,
+  retrieveRestaurantById,
   retrieveRestaurants,
+  updateRestaurantField,
 } from "../services/restaurant/restaurant.index.js";
 const router = express.Router({ mergeParams: true });
 
@@ -21,6 +23,26 @@ router.get("/retrieveRestaurants", async (req, res) => {
     const retrieveRestaurantsRes = await retrieveRestaurants(req);
     console.log("Retrieve Restaurant", retrieveRestaurantsRes);
     res.send(retrieveRestaurantsRes);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get("/retrieveRestaurantById", async (req, res) => {
+  try {
+    const retrieveRestaurantByIdRes = await retrieveRestaurantById(req);
+    console.log("Retrieve by id", retrieveRestaurantByIdRes);
+    res.send(retrieveRestaurantByIdRes);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.post("/updateField", async (req, res) => {
+  try {
+    const updateFieldRes = await updateRestaurantField(req);
+    console.log("Update res field", updateFieldRes);
+    res.send(updateFieldRes);
   } catch (e) {
     console.log(e);
   }
--- src/services/restaurant/restaurant.index.js
+++ src/services/restaurant/restaurant.index.js
@@ -8,6 +8,7 @@ export const addRestaurant = async (params) => {
   const userId = params?.body?.userId;
   const newRestaurantObject = {
     name: restaurantName,
+    lowerName: restaurantName.toLowerCase(),
     imageURL: "",
     foods: [],
     usersSubscribed: [userId],
@@ -25,6 +26,40 @@ export const retrieveRestaurants = async (params) => {
     })
     .toArray();
   return retrieveRestaurantsRes;
+};
+
+export const retrieveRestaurantById = async (params) => {
+  const restaurantId = params?.query?.restaurantId;
+  const nRestaurantId = new ObjectId(restaurantId);
+  const retrieveRes = await restaurantColl.findOne({ _id: nRestaurantId });
+  return retrieveRes;
+};
+
+export const updateRestaurantField = async (params) => {
+  const { fieldKey, fieldValue, restaurantKey } = params?.body;
+  if (!fieldKey || !fieldValue || !restaurantKey) {
+    throw new Error("Missing params");
+  }
+  const nRestaurantId = new ObjectId(restaurantKey);
+  let updateObj = {};
+  if (fieldKey === "name") {
+    updateObj[`${fieldKey}`] = fieldValue;
+    updateObj["lowerName"] = fieldValue.toLowerCase();
+  } else {
+    updateObj[`${fieldKey}`] = fieldValue;
+  }
+  const updateFieldRes = await restaurantColl.updateOne(
+    {
+      _id: nRestaurantId,
+    },
+    {
+      $set: updateObj,
+    }
+  );
+  const updatedItemObject = await restaurantColl.findOne({
+    _id: nRestaurantId,
+  });
+  return updatedItemObject;
 };
 
 export const deleteAllRestaurants = async () => {
